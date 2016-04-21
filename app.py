407fe7df5b1683de5a4c60fe21aecddb5a99f451
Ali Rashed <alitarekrashed@gmail.com>
2016-04-21 00:11:14 -0500

    got bootstrap working

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+Bootstrap(app) 
 
 @app.route('/')
 @app.route('/index')
